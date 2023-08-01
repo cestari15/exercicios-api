d91337bd767e36dbb3a6e0d7f5389d1890cbec09
cestari15 <rafacjacobs1502@gmail.com>
2023-08-01 10:08:17 -0300

    Atualizando o Exercício 5

---
+++ app/Http/Controllers/ExercicioCincoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExercicioCincoController extends Controller
+{
+    public function divisivelporquatro(Request $request)
+    {
+ 
+
+
+        if ($request->numero % 4 == 0) {
+            return json_encode([
+                'message' =>"É divisivel por 4"
+            ]);
+        } else {
+            return json_encode([
+                'message' => " Não é divisivel por 4"
+            ]);
+        }
+    }
+}

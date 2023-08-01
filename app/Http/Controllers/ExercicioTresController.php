f1c8cff89167aaf884f6c5a23802caf64cfd1025
cestari15 <rafacjacobs1502@gmail.com>
2023-08-01 10:06:01 -0300

    Atualizando o Exercício 3

---
+++ app/Http/Controllers/ExercicioTresController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExercicioTresController extends Controller
+{
+    public function media(Request $request)
+    {
+        
+
+            return json_encode([
+                'media'=>  ($request->n1 + $request->n2 +  $request->n3) / 3
+            ]);
+         
+    }
+}

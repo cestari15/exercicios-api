4a2795f1b400bf699385ed4d14939a881ef41270
cestari15 <135077858+cestari15@users.noreply.github.com>
2023-08-02 15:11:46 -0300

    Fazendo a Validação

---
+++ app/Http/Requests/ExercicioQuatroFormRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ExercicioQuatroFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'entrada' => 'require|numeric',
+        ];
+    }
+
+    public function failedValidator(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'success' => false,
+                'error' => $validator->errors()
+            ])
+        );
+    }
+
+    public function messages(){
+        return [
+            'entrada' => 'preencha o campo',
+            'entrada' => 'coloque um numero no campo'
+        ];
+    }
+}

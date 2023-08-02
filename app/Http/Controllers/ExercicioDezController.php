a164a90e9d1dd6728b12e66ce4d40a966fcedafa
cestari15 <135077858+cestari15@users.noreply.github.com>
2023-08-02 15:09:02 -0300

    Fazendo a Validação

--- app/Http/Controllers/ExercicioDezController.php
+++ app/Http/Controllers/ExercicioDezController.php
@@ -6,19 +6,19 @@ use Illuminate\Http\Request;
 
 class ExercicioDezController extends Controller
 {
-    public function impares()
-    {
+  public function impares()
+  {
 
-        $numeros = []; 
+    $numeros = [];
 
-        for ($i = 1; $i <= 80; $i++) {
-          if($i % 1 == 0){
-            array_push($numeros, $i);
-          }
-        }
-
-        return json_encode([
-            'valores'=> $numeros
-        ]);
+    for ($i = 1; $i <= 80; $i++) {
+      if ($i % 1 == 0) {
+        array_push($numeros, $i);
+      }
     }
+
+    return json_encode([
+      'valores' => $numeros
+    ]);
+  }
 }
